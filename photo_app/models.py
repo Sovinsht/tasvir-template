b8f1071633f411aea963b1fa30f994f8ea5c523f
Sovin Shrestha <Sovinsht>
2019-12-18 18:59:27 +0545

    comment added

--- photo_app/models.py
+++ photo_app/models.py
@@ -3,8 +3,16 @@ from user_app.models import UserModel
 
 # Create your models here.
 class PhotoModel(models.Model):
-    user = models.ForeignKey(UserModel, on_delete=models.CASCADE, unique=True)
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='upload_pic')
     caption = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now=True)
     like = models.PositiveIntegerField()
+
+
+      
+class CommentModel(models.Model):
+    Comment = models.TextField()
+    comment_by = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    parentpost = models.ForeignKey(PhotoModel, on_delete=models.CASCADE)
+    timestamp1 = models.DateTimeField(auto_now=True)
--- photo_app/views.py
+++ photo_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import PhotoModel
+from .models import PhotoModel,CommentModel
 
 # Create your views here.
 def index(request):
     
     upload = PhotoModel.objects.all()
-    return render(request, 'photo_app/index.html',{'upload': upload})
+    comments = CommentModel.objects.all()
+    return render(request, 'photo_app/index.html',{'upload': upload, 'comments': comments})
