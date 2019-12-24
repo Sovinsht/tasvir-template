36516a690ee616d81a698923128c7b05e10ecd24
Sovin Shrestha <Sovinsht>
2019-12-24 08:14:40 +0545

    whitenoise installed

--- Tasbir/settings.py
+++ Tasbir/settings.py
@@ -47,12 +47,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
 ]
 
 ROOT_URLCONF = 'Tasbir.urls'
@@ -132,3 +134,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 if 'DATABASE_URL' in os.environ: #means heroku 
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
